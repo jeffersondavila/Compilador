1bd5fb7bb26e8f0b55835b073034230b3fc2416b
jeffersondavila <jeffersondavila16@gmail.com>
2023-03-18 09:51:52 -0600

    [ADD] Se esta analizando cada token de la varible y se inicio con el analisis de las variables

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let codigo, lineas;
-let lista = [];
-
+let codigo, lineas, longitud, validaToken;
+let lista = [], lineaErrores = [];
 var xhttp = new XMLHttpRequest();
+let variables = /^(?=.*\b(Dim|Integer|Decimal|String|As)\b).*$/gi;
 
 // abrir una conexión con el archivo de texto
 xhttp.open("GET", "./prueba.txt", true);
@@ -13,14 +13,27 @@ xhttp.send();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         codigo = this.responseText;
-        // console.log(codigo);
 
-        // se divide cada saldo de linea
+        // se divide cada salto de linea
         lineas = codigo.split('\n');
 
         for (let i = 0; i < lineas.length; i++) {
             lista.push(lineas[i]);
         }
+
+        for (let i = 0; i < lista.length; i++) {
+            const element = lista[i];
+            let tokens = element.trim().split(" ");
+            // console.log(tokens);
+            validaToken = true;
+
+            for (let j = 0; j < tokens.length; j++) {
+                if (!variables.test(tokens[j])) {
+                    validaToken = false;
+                    lineaErrores.push(`Token inválido en línea ${i + 1}: ${tokens[j]}`);
+                }
+            }
+        }
     }
 };
-console.log(lista);
+console.log(lineaErrores);
