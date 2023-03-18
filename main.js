6d22fdde2e456d1be4e298c529ed4ba4ec30043c
jeffersondavila <jeffersondavila16@gmail.com>
2023-03-18 14:44:27 -0600

    [ADD] Se inicio con la lectura del codigo y se separo por cada salto de linea para analizar cada linea y detectar los errores

--- main.js
+++ main.js
@@ -1,39 +1,66 @@
-let codigo, lineas, longitud, validaToken;
-let lista = [], lineaErrores = [];
-var xhttp = new XMLHttpRequest();
-let variables = /^(?=.*\b(Dim|Integer|Decimal|String|As)\b).*$/gi;
+const variables = /^(?=.*\b(Dim|Integer|Decimal|String|As)\b).*$/gi;
 
-// abrir una conexión con el archivo de texto
-xhttp.open("GET", "./prueba.txt", true);
+/* Función asincrónica que leerá un archivo de texto y
+devolverá una lista de líneas de código*/
+async function obtenerLista() {
+    return new Promise((resolve, reject) => {
+        var xhttp = new XMLHttpRequest();
 
-// enviar la solicitud y leer el contenido del archivo
-xhttp.send();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                const lista = [];
+                const codigo = this.responseText;
+                // se divide cada salto de linea
+                const lineas = codigo.split('\n');
 
-// función que se llame cada vez que cambia el estado de la conexión.
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        codigo = this.responseText;
-
-        // se divide cada salto de linea
-        lineas = codigo.split('\n');
-
-        for (let i = 0; i < lineas.length; i++) {
-            lista.push(lineas[i]);
-        }
-
-        for (let i = 0; i < lista.length; i++) {
-            const element = lista[i];
-            let tokens = element.trim().split(" ");
-            // console.log(tokens);
-            validaToken = true;
-
-            for (let j = 0; j < tokens.length; j++) {
-                if (!variables.test(tokens[j])) {
-                    validaToken = false;
-                    lineaErrores.push(`Token inválido en línea ${i + 1}: ${tokens[j]}`);
+                for (let i = 0; i < lineas.length; i++) {
+                    lista.push(lineas[i]);
                 }
+
+                resolve(lista);
             }
+        };
+
+        // abrir una conexión con el archivo de texto
+        xhttp.open("GET", "./prueba.txt", true);
+        // enviar la solicitud y leer el contenido del archivo
+        xhttp.send();
+    });
+}
+
+/* Función asincrónica que analiza la lista de líneas de código
+para encontrar patrones específicos*/
+async function obtenerListaConErrores() {
+    // Obtener la lista de líneas de código
+    const lista = await obtenerLista();
+    const lineaCodigo = [], lineaErrores = [];
+
+    // Analizar cada línea de código
+    for (let i = 0; i < lista.length; i++) {
+        let tokens = [];
+        const element = lista[i];
+
+        tokens = element.trim().split(" ");
+        let lastIndex = tokens.length - 1;
+        let lastToken = tokens[lastIndex];
+        const hasPuntoComa = lastToken.endsWith(";");
+
+        // Si el último token contiene un ; se separa en otro token
+        if (hasPuntoComa) {
+            const parts = lastToken.split(";");
+            // Se agregar manualmente el punto y coma a la segunda parte
+            parts[1] = ";" + parts[1];
+            tokens.splice(lastIndex, 1, parts[0], parts[1]);
         }
+
+        lineaCodigo.push(tokens);
     }
-};
-console.log(lineaErrores);
+
+    return { lineaCodigo, lista, lineaErrores };
+}
+
+obtenerListaConErrores().then((resultado) => {
+    // console.log(resultado.lista);
+    // console.log(resultado.lineaErrores);
+    // console.log(resultado.lineaCodigo);
+});
