648c650e2ea57164a27b27e72f0a69a9be8c11a9
jeffersondavila <jeffersondavila16@gmail.com>
2023-04-15 23:39:10 -0600

    [ADD] Se creo el diseño del IDE, se procederá a cargar la información de un txt en el textarea

--- js/main.js
+++ js/main.js
@@ -1,43 +1,52 @@
 import { operadores } from "./operadores.js";
 
-const botonCargarArchivo = document.querySelector('.btn.btn-outline-secondary.mt-3.ms-2');
+const inputFile = document.createElement('input');
 const textarea = document.getElementById('floatingTextarea1');
 const textareaError = document.getElementById('floatingTextarea2');
-
-/* Función asincrónica que leerá un archivo de texto y
-devolverá una lista de líneas de código*/
-async function obtenerLista() {
-    return new Promise((resolve, reject) => {
-        var xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                const lista = [];
-                const codigo = this.responseText;
-                // se divide cada salto de linea
-                const lineas = codigo.split('\n');
-
-                for (let i = 0; i < lineas.length; i++) {
-                    lista.push(lineas[i]);
-                }
-                resolve(lista);
-            }
-        };
-        // abrir una conexión con el archivo de texto
-        xhttp.open("GET", "./js/prueba.txt", true);
-        // enviar la solicitud y leer el contenido del archivo
-        xhttp.send();
-    });
-}
+const botonCargarArchivo = document.getElementById('botonCargaCodigo');
 
 // Detectar evento de clic
-botonCargarArchivo.addEventListener('click', async function () {
-    // Obtener la lista de la función obtenerLista()
-    const lista = await obtenerLista();
-
-    // Actualizar el valor del elemento textarea con la lista
-    textarea.value = lista.join('\n');
+botonCargarArchivo.addEventListener('click', function () {
+    inputFile.type = 'file';
+    inputFile.style.display = 'none';
+    inputFile.click();
 });
+
+/* Función asincrónica que leerá un archivo de texto y devolverá una lista de líneas de código*/
+async function obtenerLista() {
+    return new Promise((resolve, reject) => {
+        inputFile.addEventListener('change', () => {
+            const archivoSeleccionado = inputFile.files[0];
+
+            if (!archivoSeleccionado || archivoSeleccionado.type !== 'text/plain') {
+                alert('Por favor, seleccione un archivo de texto (.txt)');
+                reject();
+            }
+
+            const nombreArchivo = archivoSeleccionado.name;
+
+            const xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    const lista = [];
+                    const codigo = this.responseText;
+                    // se divide cada salto de linea
+                    const lineas = codigo.split('\n');
+
+                    for (let i = 0; i < lineas.length; i++) {
+                        lista.push(lineas[i]);
+                    }
+                    resolve(lista);
+                }
+            };
+            // abrir una conexión con el archivo de texto
+            xhttp.open("GET", `./js/${nombreArchivo}`, true);
+            // enviar la solicitud y leer el contenido del archivo
+            xhttp.send();
+        });
+    });
+}
 
 /* Función asincrónica que analiza la lista de líneas de código
 para encontrar patrones específicos*/
@@ -46,6 +55,7 @@ async function obtenerListaToken() {
     const lexemas = [];
     let lastIndex, lastToken;
     const codigoFuente = await obtenerLista();
+    textarea.value = codigoFuente.join('\n');
     // Analizar cada línea de código
     for (let i = 0; i < codigoFuente.length; i++) {
         let tokens = [];
@@ -68,6 +78,7 @@ async function obtenerListaToken() {
 }
 
 obtenerListaToken().then((resultado) => {
+    let mensajeError = "";
     resultado.lexemas.map((linea, i) => {
         let fila = i + 1;
         linea.map((token, j) => {
@@ -82,9 +93,13 @@ obtenerListaToken().then((resultado) => {
             }
             let validaOperador = operadores.opAritmeticos.test(token);
             if (!validaOperador) {
-                let mensajeError = `Error en fila ${fila} columna ${(j + 1)}: ${token} no es una palabra reservada, identificador o operador válido.`;
-                textareaError.value = mensajeError;
+                if (mensajeError === "") {
+                    mensajeError = `Error en fila ${fila} columna ${(j + 1)}: ${token} no es una palabra reservada, identificador o operador válido.\n`;
+                } else {
+                    mensajeError += `Error en fila ${fila} columna ${(j + 1)}: ${token} no es una palabra reservada, identificador o operador válido.\n`;
+                }
             }
         });
+        textareaError.value = mensajeError;
     });
 });
