935fdabaf6ad5a0d9bc864568661998aebc0c5b9
jeffersondavila <jeffersondavila16@gmail.com>
2023-05-07 15:17:31 -0600

    [ADD] Se agregaron las palabras reservadas que hacían falta

--- js/operadores.js
+++ js/operadores.js
@@ -2,9 +2,9 @@ const identificadores = /^[A-Za-z]+[\_]?[A-Za-z]*[\/d]$/i;
 
 const operadoresComparacion = /^(?=.*\b(Qmas|Qopc|Q<|Q>|Q#<|Q#>|Q+)\b).*$/i
 
-const operadoresAritmeticos = /^(?=.*\b(Q#|Q@|Q~|>>|QNEL|Q>>|%%)\b).*$/i
+const operadoresAritmeticos = /^(?=.*\b(Q#|Q\?|Q@|Q~|>>|QNEL|Q>>|%%)\b).*$/i
 
-const palabrasReservadas = /^(?=.*\b(QUETZAL|CENTAVO|CHOCA|PISTO|LEN|sinPisto|Regalado|Reservado|Tapado|Quieto|Acabado)\b).*$/i
+const palabrasReservadas = /^(?=.*\b(QUETZAL|CENTAVO|CHOCA|PISTO|LEN|sinPisto|Regalado|Reservado|Tapado|Quieto|Acabado|Declaraciones|salida|paso|paso-Fin|paso-porque-paso|Repite|hastaQ|Repite-Fin|Variar|desde|fin|paso|Variar-Fin|Chispudo|Chispudo-Fin|Qpistudo|tieneCasaca|vosPlatica|coperachaMucha|queChilero|Reintegro|;|camioneta|camioneta-Fin|Fabricar|Fabricar-Fin|Principal\(\))\b).*$/i
 
 export const operadores = {
     identificadores,
