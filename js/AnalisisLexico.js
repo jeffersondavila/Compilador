711722b88c0977b946a6d2a0af9fc1a6c7b55720
jeffersondavila <jeffersondavila16@gmail.com>
2023-06-02 23:44:22 -0600

    [ADD] se agrego el if else

--- js/AnalisisLexico.js
+++ js/AnalisisLexico.js
@@ -10,6 +10,7 @@ function PrimerToken(token) {
     let subcadena = token[1];
     let validaToken = token[2];
     let finDeclaracion = token[3];
+    let sentencia = token[4];
 
     if (subcadena !== "<QC-") {
         listaErrores.push(`Error en la fila ${fila + 1} | Para declarar una instrucción se debe inicializar con "<QC-"\n`);
@@ -20,7 +21,9 @@ function PrimerToken(token) {
     }
 
     if (finDeclaracion !== ":") {
-        listaErrores.push(`Error en la fila ${fila + 1} | Después de declarar una instrucción, debe finalizar con ":"\n`);
+        if (sentencia === 0 || sentencia === 1) {
+            listaErrores.push(`Error en la fila ${fila + 1} | Después de declarar una instrucción, debe finalizar con ":"\n`);
+        }
     }
 
     return listaErrores;
@@ -34,11 +37,29 @@ function UltimoToken(token) {
 
 function SentenciaDeControl(token) {
     let validaSentencia = 0;
-    const listaSentenciasDeControl = [/paso/g, /paso-porque-paso/g, /paso-Fin/g];
+    const listaSentenciasDeControl1 = [/paso-Fin/g];
+    const listaSentenciasDeControl2 = [/paso/g];
+    const listaSentenciasDeControl3 = [/paso-porque-paso/g];
 
-    for (const lista of listaSentenciasDeControl) {
+    for (const lista of listaSentenciasDeControl1) {
         if (lista.test(token)) {
-            validaSentencia = 1;
+            validaSentencia = 11;
+        }
+    }
+
+    if (validaSentencia === 0) {
+        for (const lista of listaSentenciasDeControl3) {
+            if (lista.test(token)) {
+                validaSentencia = 12;
+            }
+        }
+    }
+
+    if (validaSentencia === 0) {
+        for (const lista of listaSentenciasDeControl2) {
+            if (lista.test(token)) {
+                validaSentencia = 1;
+            }
         }
     }
     return validaSentencia;
@@ -65,6 +86,7 @@ function ValidarCodigo(lineas) {
         const arregloInterior = lineas[fila];
         let declaraInstruccion = "";
         let estado = 0;
+        let sentencia;
 
         for (let indice = 0; indice < arregloInterior.length; indice++) {
             const elemento = arregloInterior[indice];
@@ -74,14 +96,32 @@ function ValidarCodigo(lineas) {
             }
 
             if (indice === 0) {
+                let instruccion;
                 const longitudElemento = elemento.length;
                 const posicionGuion = elemento.indexOf("-") + 1;
                 const subcadena = elemento.substring(0, posicionGuion);
                 const posicionDoblePunto = elemento.indexOf(":");
-                const instruccion = elemento.substring(posicionGuion, posicionDoblePunto);
+
+                if (posicionDoblePunto !== -1) {
+                    instruccion = elemento.substring(posicionGuion, posicionDoblePunto);
+                } else {
+                    instruccion = elemento.substring(posicionGuion, longitudElemento);
+                }
+
+                sentencia = SentenciaDeControl(instruccion);
+
+                if (sentencia === 0) {
+                    sentencia = Ciclos(instruccion);
+                }
+
+                if (instruccion.endsWith(">")) {
+                    instruccion = instruccion.replace(/>$/, "");
+                }
+
                 const finDeclaracion = elemento.substring(longitudElemento - 1, longitudElemento);
                 const validaToken = operadores.palabrasReservadas.test(instruccion);
-                let contenido = [fila, subcadena, validaToken, finDeclaracion];
+                let contenido = [fila, subcadena, validaToken, finDeclaracion, sentencia];
+
                 let resultado = PrimerToken(contenido);
                 let longitudResultado = resultado.length;
 
@@ -92,25 +132,62 @@ function ValidarCodigo(lineas) {
                 estado++;
                 let validaToken;
 
-                switch (estado) {
-                    case 1: // Variable
-                        validaToken = operadores.palabrasReservadas.test(elemento);
-                        if (!validaToken) {
-                            listaErrores.push(`Error en la fila ${fila + 1} | La palabra reservada no es válida\n`);
-                        }
-                        break;
-                    case 2: // Identificadores
-                        validaToken = operadores.identificadores.test(elemento);
-                        if (!validaToken) {
-                            listaErrores.push(`Error en la fila ${fila + 1} | El identificador no es válido\n`);
-                        }
-                        break;
-                    case 3: // Asignación
-                        validaToken = operadores.operadoresAritmeticos.test(elemento);
-                        if (!validaToken) {
-                            listaErrores.push(`Error en la fila ${fila + 1} | El operador no es válido\n`);
-                        }
-                        break;
+                if (sentencia === 0) {
+                    switch (estado) {
+                        case 1: // Variable
+                            validaToken = operadores.palabrasReservadas.test(elemento);
+                            if (!validaToken) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | La palabra reservada no es válida\n`);
+                            }
+                            break;
+                        case 2: // Identificadores
+                            validaToken = operadores.identificadores.test(elemento);
+                            if (!validaToken) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | El identificador no es válido\n`);
+                            }
+                            break;
+                        case 3: // Asignación
+                            validaToken = operadores.operadoresAritmeticos.test(elemento);
+                            if (!validaToken) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | El operador no es válido\n`);
+                            }
+                            break;
+                    }
+                } else if (sentencia === 1) {
+                    switch (estado) {
+                        case 1: // Variable
+                            let tokenElemento;
+                            let tokenInicio = elemento.startsWith("[");
+                            let longitud = elemento.length;
+
+                            if (tokenInicio) {
+                                tokenElemento = elemento.substring(1, longitud);
+                            } else {
+                                tokenElemento = elemento.substring(0, longitud);
+                            }
+                            if (!tokenInicio) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | La condicion no es valida\n`);
+                            }
+
+                            validaToken = operadores.identificadores.test(tokenElemento);
+
+                            if (!validaToken) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | El identificador no es válida\n`);
+                            }
+                            break;
+                        case 2: // Identificadores
+                            validaToken = operadores.operadoresComparacion.test(elemento);
+                            if (!validaToken) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | El signo de comparacion no es válido\n`);
+                            }
+                            break;
+                        case 3: // Asignación
+                            validaToken = operadores.identificadores.test(elemento);
+                            if (!validaToken) {
+                                listaErrores.push(`Error en la fila ${fila + 1} | El identificador no es válida\n`);
+                            }
+                            break;
+                    }
                 }
             } else {
                 const indiceCierre = elemento.length;
@@ -118,35 +195,49 @@ function ValidarCodigo(lineas) {
                 const instruccionCierre = elemento.substring(indiceCierre - 1, indiceCierre);
                 let validaToken, validaString;
 
+                if (instruccionCierre !== ">") {
+                    listaErrores.push(`Error en la fila ${fila} | Para finalizar una instrucción se debe agregar la terminación ">"\n`);
+                }
                 if (cadenaIdentificador.endsWith(">")) {
                     cadenaIdentificador = cadenaIdentificador.replace(/>$/, "");
                 }
 
-                if (cadenaIdentificador.startsWith('"') && cadenaIdentificador.endsWith('"')) {
-                    validaString = true;
-                }
-
-                if (declaraInstruccion === "Declaraciones" || declaraInstruccion === "salida" && !validaString) {
-                    if (!cadenaIdentificador.startsWith('"')) {
-                        validaToken = operadores.identificadores.test(cadenaIdentificador);
+                if (sentencia === 0) {
+                    if (cadenaIdentificador.startsWith('"') && cadenaIdentificador.endsWith('"')) {
+                        validaString = true;
                     }
-                }
 
-                if (instruccionCierre !== ">") {
-                    listaErrores.push(`Error en la fila ${fila} | Para finalizar una instrucción se debe agregar la terminación ">"\n`);
-                }
+                    if (declaraInstruccion === "Declaraciones" || declaraInstruccion === "salida" && !validaString) {
+                        if (!cadenaIdentificador.startsWith('"')) {
+                            validaToken = operadores.identificadores.test(cadenaIdentificador);
+                        }
+                    }
 
-                if (validaToken === false) {
-                    listaErrores.push(`Error en la fila ${fila} | El identificador no es valido\n`);
-                }
+                    if (validaToken === false) {
+                        listaErrores.push(`Error en la fila ${fila} | El identificador no es valido\n`);
+                    }
 
-                if (validaString === false) {
-                    listaErrores.push(`Error en la fila ${fila} | La palabra reservada no es valida\n`);
+                    if (validaString === false) {
+                        listaErrores.push(`Error en la fila ${fila} | La palabra reservada no es valida\n`);
+                    }
+                } else if (sentencia === 1) {
+                    let indiceif = cadenaIdentificador.length;
+                    let cadenaif = cadenaIdentificador.substring(0, indiceif - 1);
+
+                    if (!cadenaIdentificador.endsWith("]")) {
+                        listaErrores.push(`Error en la fila ${fila + 1} | La condicion no es valida\n`);
+                    }
+
+                    validaToken = operadores.identificadores.test(cadenaif);
+
+                    if (!validaToken) {
+                        listaErrores.push(`Error en la fila ${fila + 1} | El signo de comparacion no es válido\n`);
+                    }
                 }
             }
         }
         // Cambia la linea que se esta iterando del código
-        fila += 1;
+        // fila += 1;
         declaraInstruccion = "";
     }
 
